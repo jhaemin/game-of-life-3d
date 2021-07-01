acefd9fa6e7355acf68d0ffe8b70dc45faec5c39
Jang Haemin <io@jhaemin.com>
2021-07-02 01:13:17 +0900

    Migrate from vite to cra

---
+++ README.md
@@ -0,0 +1,1 @@
+# Game of Life 3D
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { RecoilRoot } from 'recoil'
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 )
