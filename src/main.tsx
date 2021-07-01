7d1d702272d27f2f0ac2d27db4564318187c7cc1
Jang Haemin <io@jhaemin.com>
2021-07-01 22:14:30 +0900

    Wrap the app with recoil root

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { RecoilRoot } from 'recoil'
 import App from './App'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
 )
