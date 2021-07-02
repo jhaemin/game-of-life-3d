46adb8337fb827edb6d3dd1ab43011a441b34b08
Jang Haemin <io@jhaemin.com>
2021-07-02 22:21:00 +0900

    Updates

---
+++ _next/static/lgwvuhEAuYJQTZt3ViXbk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4d8026457c98a017207c.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
