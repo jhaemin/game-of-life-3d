c2fe2a5e684f3aabd36399b90360d6a764ca43e5
Jang Haemin <io@jhaemin.com>
2021-07-02 22:25:37 +0900

    Add asset prefix in favour of github pages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   env: {
     PUBLIC_URL: '',
   },
+  assetPrefix: '/game-of-life-3d/',
   // Remove this to leverage Next.js' static image handling
   // read more here: https://nextjs.org/docs/api-reference/next/image
   images: {
