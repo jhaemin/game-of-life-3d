451428aa49baf752a355a43a20f1c66f2011a3b3
Jang Haemin <io@jhaemin.com>
2021-07-02 16:36:48 +0900

    Entirely reimplement using native three.js
    - Use camera controls instead of orbit controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,18 @@
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import 'muyaho'
-import { useRef } from 'react'
-import THREE from 'three'
+import { useEffect } from 'react'
 import { useSnapshot } from 'valtio'
-import { useWindowSize } from './hooks/use-window-size'
 import { state } from './state'
 
-// Asynchronously import WebAssembly
-import('./wasm').catch((e) => console.error(e))
-
-function Box(props: JSX.IntrinsicElements['mesh']) {
-  const mesh = useRef<THREE.Mesh>(null!)
-
-  return (
-    <mesh {...props} ref={mesh}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="crimson" />
-    </mesh>
-  )
-}
-
 function App() {
-  const { width, height } = useWindowSize()
-  const { positions } = useSnapshot(state)
+  const { windowSize } = useSnapshot(state)
+
+  useEffect(() => {
+    // Asynchronously import WebAssembly
+    import('./wasm').catch((e) => console.error(e))
+  }, [])
 
   return (
     <div className="app">
-      <Canvas style={{ width, height }}>
-        <PerspectiveCamera position={[0, 0, 0]}>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          {positions.map((position, i) => (
-            <Box key={i} position={position} />
-          ))}
-          <OrbitControls
-            enableDamping
-            zoomSpeed={0.8}
-            rotateSpeed={0.5}
-            minDistance={0}
-            maxDistance={100}
-          />
-        </PerspectiveCamera>
-      </Canvas>
+      <canvas id="canvas" {...windowSize} />
     </div>
   )
 }
--- src/wasm.ts
+++ src/wasm.ts
@@ -1,37 +1,115 @@
-import { Vector3 } from '@react-three/fiber'
-import { Universe, Cell } from 'wasm-game-of-life'
+import '@react-three/fiber'
+import CameraControls from 'camera-controls'
+import * as THREE from 'three'
+import { Cell, Universe } from 'wasm-game-of-life'
 import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg.wasm'
 import { state } from './state'
 
-const width = 64
-const height = 64
+CameraControls.install({ THREE })
+
+const { windowSize } = state
+const { width: canvasWidth, height: canvasHeight } = windowSize
+
+const universeWidth = 64
+const universeHeight = 64
 
 function getIndex(row: number, column: number) {
-  return row * width + column
+  return row * universeWidth + column
 }
 
-const universe = Universe.new(width, height)
+const canvas = document.querySelector<HTMLCanvasElement>('canvas')!
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xffffff)
+
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(canvasWidth, canvasHeight)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+const camera = new THREE.PerspectiveCamera(75, canvasWidth / canvasHeight)
+camera.position.set(0, 0, 10)
+
+// Create camera controls after setting the camera position
+const cameraControls = new CameraControls(camera, canvas)
+
+cameraControls.minDistance = 10
+cameraControls.dollySpeed = 0.5
+cameraControls.maxDistance = 80
+cameraControls.dolly(-50)
+
+const light1 = new THREE.DirectionalLight(0xffffff, 2)
+light1.position.set(90, 80, 150)
+const light2 = new THREE.DirectionalLight(0xffffff, 2)
+light2.position.set(-30, -50, -100)
+scene.add(light1, light2)
+
+renderer.render(scene, camera)
+
+window.addEventListener('resize', () => {
+  windowSize.width = window.innerWidth
+  windowSize.height = window.innerHeight
+
+  camera.aspect = windowSize.width / windowSize.height
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(windowSize.width, windowSize.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+  renderer.render(scene, camera)
+})
+
+const universe = Universe.new(universeWidth, universeHeight)
 const cellsPtr = universe.cells()
 
-function tick() {
+const meshes: THREE.Mesh[] = Array(universeWidth * universeHeight).fill(
+  undefined
+)
+
+for (let row = 0; row < universeHeight; row++) {
+  for (let col = 0; col < universeWidth; col++) {
+    const index = getIndex(row, col)
+    meshes[index] = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshStandardMaterial({ color: 'crimson' })
+    )
+    meshes[index].position.set(
+      (col - universeWidth / 2) * 1.4,
+      (row - universeHeight / 2) * 1.4,
+      0
+    )
+  }
+}
+
+scene.add(...meshes)
+
+const tick = () => {
+  // const delta = clock.getDelta()
+  // const hasControlsUpdated = cameraControls.update(delta)
+
   universe.tick()
 
-  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height)
-  const positions: Vector3[] = []
+  const cells = new Uint8Array(
+    memory.buffer,
+    cellsPtr,
+    universeWidth * universeHeight
+  )
 
-  for (let row = 0; row < height; row++) {
-    for (let col = 0; col < width; col++) {
+  for (let row = 0; row < universeHeight; row++) {
+    for (let col = 0; col < universeWidth; col++) {
       const index = getIndex(row, col)
+      const material = meshes[index].material as any
 
       if (cells[index] === Cell.Alive) {
-        positions.push([(col - width / 2) * 2, (row - height / 2) * 2, 0])
+        material.transparent = false
+        material.opacity = 1
+      } else {
+        material.transparent = true
+        material.opacity = 0
       }
     }
   }
 
-  state.positions = positions
-
+  renderer.render(scene, camera)
   requestAnimationFrame(tick)
 }
 
-requestAnimationFrame(tick)
+tick()
