bb2c53d17e85626ec640dd12a83ab43d0ee469a9
Jang Haemin <io@jhaemin.com>
2021-07-03 12:09:17 +0900

    Improve performance

--- src/wasm.ts
+++ src/wasm.ts
@@ -1,5 +1,6 @@
 import CameraControls from 'camera-controls'
 import * as THREE from 'three'
+import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils'
 import { Cell, Universe } from '../wasm/pkg/wasm_game_of_life_bg'
 import { memory } from '../wasm/pkg/wasm_game_of_life_bg.wasm'
 import { state } from './state'
@@ -64,23 +65,44 @@ const cellsPtr = universe.cells()
 const meshes: THREE.Mesh[] = Array(universeWidth * universeHeight).fill(
   undefined
 )
+let geometries: THREE.BufferGeometry[] = []
 
 for (let row = 0; row < universeHeight; row++) {
   for (let col = 0; col < universeWidth; col++) {
-    const index = getIndex(row, col)
-    meshes[index] = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial({ color: 'crimson' })
-    )
-    meshes[index].position.set(
+    // const index = getIndex(row, col)
+    // meshes[index] = new THREE.Mesh(
+    //   new THREE.BoxGeometry(1, 1, 1),
+    //   new THREE.MeshStandardMaterial({ color: 'crimson' })
+    // )
+    // meshes[index].position.set(
+    //   (col - universeWidth / 2) * 1.4,
+    //   (row - universeHeight / 2) * 1.4,
+    //   0
+    // )
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    geometry.translate(
       (col - universeWidth / 2) * 1.4,
       (row - universeHeight / 2) * 1.4,
       0
     )
+
+    geometries.push(geometry)
   }
 }
 
-scene.add(...meshes)
+const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(
+  geometries,
+  false
+)
+const mesh = new THREE.Mesh(
+  mergedGeometry,
+  new THREE.MeshStandardMaterial({ color: 'crimson' })
+)
+
+scene.add(mesh)
+
+// scene.add(...meshes)
 
 const tick = () => {
   const delta = clock.getDelta()
@@ -94,20 +116,43 @@ const tick = () => {
     universeWidth * universeHeight
   )
 
+  geometries = []
+
   for (let row = 0; row < universeHeight; row++) {
     for (let col = 0; col < universeWidth; col++) {
       const index = getIndex(row, col)
-      const material = meshes[index].material as any
+      // const material = meshes[index].material as any
+
+      // if (cells[index] === Cell.Alive) {
+      //   material.transparent = false
+      //   material.opacity = 1
+      // } else {
+      //   material.transparent = true
+      //   material.opacity = 0
+      // }
+
+      // const geometry = geometries[index]
+      // geometry.dispose()
+
+      // if (cells[index] === Cell.Alive) {
+      //   geometry.scale(1, 1, 1)
+      // } else {
+      //   geometry.scale(0, 0, 0)
+      // }
 
       if (cells[index] === Cell.Alive) {
-        material.transparent = false
-        material.opacity = 1
-      } else {
-        material.transparent = true
-        material.opacity = 0
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        geometry.translate(
+          (col - universeWidth / 2) * 1.4,
+          (row - universeHeight / 2) * 1.4,
+          0
+        )
+        geometries.push(geometry)
       }
     }
   }
+
+  mesh.geometry = BufferGeometryUtils.mergeBufferGeometries(geometries, false)
 
   renderer.render(scene, camera)
   requestAnimationFrame(tick)
